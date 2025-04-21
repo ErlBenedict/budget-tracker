6e262b3a018994b67a308c9992e2e5487e1bde17
ErlBenedict <Erlbenedictsantos21@gmail.com>
2025-04-21 22:31:05 +0800

    Fix: removed duplicate code in __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -28,12 +27,6 @@ def create_app():
 
     create_database(app)
 
-    # Avoid circular imports with app context
-    with app.app_context():
-        if not path.exists("website/" + DB_NAME):
-            db.create_all()
-            print("Database Created!")
-
     # Flask-Login Configuration
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
@@ -49,56 +42,3 @@ def create_database(app):
     if not path.exists('website/' + DB_NAME):
         with app.app_context():
             db.create_all()
-=======
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from flask_migrate import Migrate
-from os import path
-
-db = SQLAlchemy()
-migrate = Migrate()
-DB_NAME = "database.db"
-
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'JESUS IS LORD'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    app.config.from_object('config.Config')
-
-    db.init_app(app)
-    migrate.init_app(app, db)  # 👈 important!
-
-    from .auth import auth
-    from .views import views
-
-    app.register_blueprint(auth, url_prefix='/')
-    app.register_blueprint(views, url_prefix='/')
-
-    from .models import User, Expense, Budget
-
-    create_database(app)
-
-    # Avoid circular imports with app context
-    with app.app_context():
-        if not path.exists("website/" + DB_NAME):
-            db.create_all()
-            print("Database Created!")
-
-    # Flask-Login Configuration
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
-
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
-
-    return app
-
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        with app.app_context():
-            db.create_all()
->>>>>>> 11087191ba6c9c67c5ba75a139af05d657e2bf97
-        print('Database Created!')
